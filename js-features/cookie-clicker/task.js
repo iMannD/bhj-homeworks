b33792a6631aa21d4fe451d0b7e9389ccfd877ca
iMannD <122105359+iMannD@users.noreply.github.com>
2023-04-19 22:51:35 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,19 +1,9 @@
-const cookieImage = document.getElementById("cookie");
-let cookieClick = document.getElementById("clicker__counter");
-let cookieSpeedClick = document.getElementById("clicker__speed__counter");
-let todayTime = Date.now();
-let previousClicks = 0;
+const picture = document.querySelector('.clicker__cookie');
+//console.log(picture)
+let counter = document.getElementById("clicker__counter").innerHTML;
 
-cookieImage.onclick = function () {
-    // new image size
-    cookieImage.classList.contains('clicker__cookie') ? cookieImage.className = 'new__clicker__cookie' : cookieImage.className = 'clicker__cookie';
-
-    // clicks count
-    let currentClicks = parseInt(cookieClick.textContent);
-    cookieClick.textContent = currentClicks + 1;
-
-    // click speed count
-    cookieSpeedClick.textContent = (((Date.now() - todayTime) / 1000) / (currentClicks - previousClicks)).toFixed(2);
-    previousClicks = currentClicks;
-
-};
+picture.onclick = () => {
+   counter++
+   picture.width == 200 ? (picture.width = 300) : (picture.width = 200);
+   document.getElementById("clicker__counter").innerHTML = counter;
+}
