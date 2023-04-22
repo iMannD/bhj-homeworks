47b85376a8da6d3853a425ee11dc50b76c3a3e0f
iMannD <122105359+iMannD@users.noreply.github.com>
2023-04-22 13:27:02 +0300

    11

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,3 +1,8 @@
 document.getElementById('modal_main').classList.add('modal_active')
 let activePop = document.getElementsByClassName('modal_active')
-activePop.getElementsByClassName('modal__close').onclick = activePop.classList.remove('modal_active')
+activePop.getElementsByClassName('modal__close')[0].onclick = activePop.classList.remove('modal_active')
+// так как больше одного такого в одном диве не будет
+
+let showSuccess = document.querySelector('show-success')
+showSuccess.onclick = document.getElementById('modal_success').classList.add('modal_active')
+
