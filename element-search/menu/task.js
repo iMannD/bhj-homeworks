a6e6649fe40063cb555630dc35a3105f18cd61b2
iMannD <122105359+iMannD@users.noreply.github.com>
2023-04-23 01:24:25 +0300

    Update task.js
    Tt

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,9 +1,28 @@
-let menuLink = Array.from(document.getElementsByClassName('menu__link'));
+const arrSlides = Array.from(document.querySelectorAll('.slider__item'));
+const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
+const sliderArrowNext = document.querySelector('.slider__arrow_next');
 
-menuLink.forEach(item => item.addEventListener('click', function(evt) {
-  if ((item.parentElement.getElementsByClassName('menu_sub')).length == 1) {
-    evt.preventDefault();
-    let menuSub = Array.from(item.parentElement.getElementsByClassName('menu_sub'));
-    menuSub.forEach(sub => sub.classList.toggle('menu_active'));
-    }
-}));
+sliderArrowNext.onclick = function() {
+  slideIndex = arrSlides.findIndex(value => value === document.querySelector('.slider__item_active'));
+  showSlides(slideIndex += 1);         
+};
+
+sliderArrowPrev.onclick = function() {   
+  slideIndex = arrSlides.findIndex(value => value === document.querySelector('.slider__item_active'));
+  showSlides(slideIndex -= 1);
+};
+
+function showSlides(n) {
+  const slideIndex = arrSlides.findIndex(value => value === document.querySelector('.slider__item_active'));
+  arrSlides[slideIndex].className = "slider__item";
+
+  if (n < 0) {
+    n = (arrSlides.length - 1);
+  }
+
+  if (n > (arrSlides.length - 1)) {
+    n = 0;
+  }
+
+  arrSlides[n].className = "slider__item slider__item_active";
+}
