8bd4305138126ad0b81b616526d6e202dc327056
iMannD <122105359+iMannD@users.noreply.github.com>
2023-04-23 14:28:24 +0300

    Update task.js

---
+++ event-object/tabs/task.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const arrTabs = Array.from(document.querySelectorAll(".tab"));
+const arrTabContents = Array.from(document.querySelectorAll(".tab__content"));
+
+arrTabs.forEach(tab => {
+  tab.addEventListener("click", (e) => {
+    arrTabs.forEach(element => {
+      if (element.closest(".tabs") === e.target.closest(".tabs")) {
+        element.classList.remove("tab_active");
+      }
+    });
+
+    e.target.classList.add("tab_active");
+
+    arrTabContents.forEach(element => {
+      if (element.closest(".tabs") === e.target.closest(".tabs")) {
+        element.classList.remove("tab__content_active");
+      }
+    });
+
+    const indexTab = arrTabs.indexOf(tab);
+    arrTabContents[indexTab].classList.add("tab__content_active");
+  });
+});
